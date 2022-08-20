f3ab808fb1ef6ee1748b8cb0cd6cc89f4b8ea3e5
Tomás Catena <tomascatena@gmail.com>
2022-08-20 20:07:53 +0200

    add simple component to write code and submit it

--- packages/local-client/src/App.tsx
+++ packages/local-client/src/App.tsx
@@ -2,15 +2,35 @@ import React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import { useDarkMode } from './hooks/useDarkMode';
 import CssBaseline from '@mui/material/CssBaseline';
+import { Container } from '@mui/material';
 
 const App = () => {
   const { theme } = useDarkMode();
+
+  const [input, setInput] = React.useState('');
+  const [code, setCode] = React.useState('');
+
+  const onClick = () => {
+    console.log(input);
+  };
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
 
-      <h1>Local Client!</h1>
+      <Container>
+        <textarea
+          onChange={e => setInput(e.target.value)}
+          value={input}
+          cols={30} rows={10}
+        ></textarea>
+
+        <div>
+          <button onClick={onClick}>Submit</button>
+        </div>
+
+        <pre>{code}</pre>
+      </Container>
     </ThemeProvider>
   );
 };
