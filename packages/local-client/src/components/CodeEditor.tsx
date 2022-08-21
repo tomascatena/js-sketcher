4fcc351aca037a629349d7118b74df1df1b1ecce
Tomás Catena <tomascatena@gmail.com>
2022-08-21 19:51:50 +0200

    add props to customize Monaco Editor

--- packages/local-client/src/components/CodeEditor.tsx
+++ packages/local-client/src/components/CodeEditor.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
-import Editor, { EditorProps } from "@monaco-editor/react";
+import Editor from "@monaco-editor/react";
 
 type Props = {
-  language?: EditorProps['language'];
+  language?: string;
+  theme?: 'vs-dark' | 'light';
 };
 
 const CodeEditor = ({
-  language = 'javascript'
+  language = 'javascript',
+  theme = 'vs-dark'
 }: Props) => {
   return (
     <Editor
       height={500}
       language={language}
+      theme={theme}
+      options={{
+        wordWrap: 'on',
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true
+      }}
     />
   );
 };
