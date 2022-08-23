8c374d7ff76a34aa8a2658e2da039c481d970664
Tomás Catena <tomascatena@gmail.com>
2022-08-23 20:47:07 +0200

    conditionally render Editor or MD

--- packages/local-client/src/components/TextCell/TextCell.tsx
+++ packages/local-client/src/components/TextCell/TextCell.tsx
@@ -1,14 +1,38 @@
 import React from 'react';
-import TextEditor from './TextEditor/TextEditor';
+import MDEditor from '@uiw/react-md-editor';
 
 type Props = {};
 
 const TextCell = (props: Props) => {
-  return (
-    <div>
-      <TextEditor />
-    </div>
-  );
+  const [isEditing, setIsEditing] = React.useState(false);
+  const [value, setValue] = React.useState("# Tomas Catena");
+
+  React.useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      setIsEditing(false);
+    };
+
+    window.addEventListener('click', listener, { capture: true });
+
+    return () => {
+      window.removeEventListener('click', listener, { capture: true });
+    };
+  }, []);
+
+  if (isEditing) {
+    return (
+      <MDEditor
+        value={value}
+        onChange={e => setValue(e as string)}
+      />
+    );
+  } else {
+    return (
+      <div onClick={() => setIsEditing(true)}>
+        <MDEditor.Markdown source={value} />
+      </div>
+    );
+  }
 };
 
 export default TextCell;
