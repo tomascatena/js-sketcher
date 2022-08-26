d51c8cabf851ed1411cc20ac9d5f1aa7a78e5b89
Tomás Catena <tomascatena@gmail.com>
2022-08-26 13:58:57 +0200

    add definition for Cell and bundle actions

--- packages/local-client/src/store/features/bundles/bundlesSlice.ts
+++ packages/local-client/src/store/features/bundles/bundlesSlice.ts
@@ -1,4 +1,11 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+export interface Cell {
+  id: string;
+  direction: 'up' | 'down';
+  type: 'javascript' | 'markdown';
+  content: string;
+}
 
 export interface BundlesState {
   value: number;
@@ -13,7 +20,13 @@ const initialState: BundlesState = {
 export const bundlesSlice = createSlice({
   name: 'bundles',
   initialState,
-  reducers: {},
+  reducers: {
+    moveCell: (state, action: PayloadAction<{ id: string; to: string }>) => {},
+    deleteCell: (state, action: PayloadAction<{ id: string }>) => {},
+    insertCellBefore: (state, action: PayloadAction<{ id: string; before: string }>) => {},
+    insertCellAfter: (state, action: PayloadAction<{ id: string; after: string }>) => {},
+    updateCell: (state, action: PayloadAction<{ id: string; value: string }>) => {},
+  },
 });
 
 export const bundlesActions = bundlesSlice.actions;
