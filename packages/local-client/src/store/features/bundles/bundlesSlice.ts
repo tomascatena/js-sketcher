af1909851841a4414f6c645fe956e23bdb9cf1c4
Tomás Catena <tomascatena@gmail.com>
2022-08-26 14:42:17 +0200

    add updateCell action

--- packages/local-client/src/store/features/bundles/bundlesSlice.ts
+++ packages/local-client/src/store/features/bundles/bundlesSlice.ts
@@ -1,32 +1,24 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-export interface Cell {
-  id: string;
-  direction: 'up' | 'down';
-  type: 'javascript' | 'markdown';
-  content: string;
+export interface Bundle {
+  code: string;
+  error: string | null;
 }
 
 export interface BundlesState {
-  value: number;
-  status: 'idle' | 'loading' | 'failed';
+  currentCellId: string | null;
+  cellBundles: { [key: string]: Bundle };
 }
 
 const initialState: BundlesState = {
-  value: 0,
-  status: 'idle',
+  currentCellId: null,
+  cellBundles: {},
 };
 
 export const bundlesSlice = createSlice({
   name: 'bundles',
   initialState,
-  reducers: {
-    moveCell: (state, action: PayloadAction<{ id: string; to: string }>) => {},
-    deleteCell: (state, action: PayloadAction<{ id: string }>) => {},
-    insertCellBefore: (state, action: PayloadAction<{ id: string; before: string }>) => {},
-    insertCellAfter: (state, action: PayloadAction<{ id: string; after: string }>) => {},
-    updateCell: (state, action: PayloadAction<{ id: string; value: string }>) => {},
-  },
+  reducers: {},
 });
 
 export const bundlesActions = bundlesSlice.actions;
--- packages/local-client/src/store/features/cells/cellsSlice.ts
+++ packages/local-client/src/store/features/cells/cellsSlice.ts
@@ -1,19 +1,48 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+export type Direction = 'up' | 'down';
+export type CellType = 'javascript' | 'markdown';
+
+export interface Cell {
+  id: string;
+  direction: Direction;
+  type: CellType;
+  content: string;
+}
 
 export interface CellsState {
-  value: number;
-  status: 'idle' | 'loading' | 'failed';
+  data: { [key: string]: Cell };
+  order: string[];
+  loading: boolean;
+  error: string | null;
 }
 
 const initialState: CellsState = {
-  value: 0,
-  status: 'idle',
+  data: {},
+  order: [],
+  loading: false,
+  error: null,
 };
 
 export const cellsSlice = createSlice({
   name: 'cells',
   initialState,
-  reducers: {},
+  reducers: {
+    moveCell: (state, action: PayloadAction<{ id: string; direction: Direction }>) => {},
+    deleteCell: (state, action: PayloadAction<{ id: string }>) => {
+      const { id } = action.payload;
+      delete state.data[id];
+
+      state.order = state.order.filter((cellId) => cellId !== id);
+    },
+    insertCellBefore: (state, action: PayloadAction<{ id: string; cellType: CellType }>) => {},
+    insertCellAfter: (state, action: PayloadAction<{ id: string; cellType: CellType }>) => {},
+    updateCell: (state, action: PayloadAction<{ id: string; content: string }>) => {
+      const { id, content } = action.payload;
+
+      state.data[id].content = content;
+    },
+  },
 });
 
 export const cellsActions = cellsSlice.actions;
