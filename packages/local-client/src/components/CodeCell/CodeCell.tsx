7ee113fd7d70424526f7668fbc039096cacfc91b
Tomás Catena <tomascatena@gmail.com>
2022-09-09 19:24:26 +0200

    add show function

--- packages/local-client/src/components/CodeCell/CodeCell.tsx
+++ packages/local-client/src/components/CodeCell/CodeCell.tsx
@@ -33,7 +33,26 @@ const CodeCell = ({ cell }: Props) => {
   const cumulativeCode = useTypedSelector(state => {
     const { data, order } = state.cells;
     const orderedCells = order.map(id => data[id]);
-    const cumulativeCode = [];
+    const cumulativeCode = [
+      `
+      import _React from 'react';
+      import _ReactDOM from 'react-dom';
+
+      const show = (value) => {
+        const root = document.querySelector('#root');
+
+        if(typeof value === 'object') {
+          if(value.$$typeof && value.props) {
+            _ReactDOM.render(value, root);
+          } else {
+            root.innerHTML = JSON.stringify(value);
+          }
+        } else {
+          root.innerHTML = value;
+        }
+      };
+      `
+    ];
 
     for (let c of orderedCells) {
       if (c.type === CellType.JAVASCRIPT) {
