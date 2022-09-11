01ef86d8c52dc61fa0bcf82f5a519b66c85a935b
Tomás Catena <tomascatena@gmail.com>
2022-09-11 09:20:03 +0200

    improve error handling

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -6,20 +6,42 @@ type Options = {
   port: string;
 };
 
+type LocalApiError = {
+  code: string;
+};
+
 export const serveCommand = new Command()
   .command('serve [filename]')
   .description('Open a file for editing')
   .option('-p, --port <number>', 'port to run server on', '4050')
   .action(async (filename = 'notebook.js', options: Options) => {
+    const isLocalApiError = (err: any): err is LocalApiError => {
+      return typeof err.code === 'string';
+    };
+
     try {
       await serve({
         port: Number(options.port),
         filename: path.basename(filename),
         dir: path.join(process.cwd(), path.dirname(filename)),
       });
-    } catch (error) {
-      if (error instanceof Error) {
-        console.log('Here is the error', error.message);
+
+      console.log(
+        `Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file.`
+      );
+    } catch (err) {
+      if (isLocalApiError(err)) {
+        if (err.code === 'EADDRINUSE') {
+          console.error('Port is in use. Try running on a different port.');
+        } else if (err.code === 'EACCES') {
+          console.error('Port requires elevated privileges.');
+        } else if (err instanceof Error) {
+          console.error('Here is the problem', err.message);
+        }
+      } else if (err instanceof Error) {
+        console.error('Here is the problem', err.message);
       }
+
+      process.exit(1);
     }
   });
